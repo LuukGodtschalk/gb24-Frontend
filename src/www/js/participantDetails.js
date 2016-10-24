2c41384ba48b4df70c4ce6ca5721cadb47f2acf7
Luuk Godtschalk <godtschalk.luuk@gmail.com>
2016-10-24 18:25:22 +0200

    Add average lap time and display time of first lap * Known issues: lap time does not display hours, so XhYYmZZs shows as YY:ZZ

--- src/www/js/participantDetails.js
+++ src/www/js/participantDetails.js
@@ -5,11 +5,27 @@ app.controller('gb24ParticipantDetails', ['$scope', 'eventManager', '$routeParam
   $scope.data = [];
   eventManager.watch('participants/' + participantId, function (msg) {
     var laps = msg.data.laps;
+    var lapTotal = 0;
     for (var n = 0; n < laps.length; n++) {
       laps[n].lapnum = n+1;
+      var lapStart;
+      var lapEnd = new Date(laps[n].time);
       if (n>0) {
-        laps[n].laptime = (new Date(laps[n].time)) - (new Date(laps[n-1].time));
+        lapStart = new Date(laps[n-1].time);
+      } else {
+        //TODO: use start time from categories.json
+        console.log(msg.data);
+        if (msg.data.categoryId === 4) {
+          lapStart = new Date(2016, 8, 25, 8, 0);
+        } else {
+          lapStart = new Date(2016, 8, 24, 12, 59, 40);
+        }
       }
+      laps[n].laptime = lapEnd - lapStart;
+      lapTotal += laps[n].laptime;
+    }
+    if (laps.length > 0) {
+      msg.data.lapAvg = lapTotal / laps.length;
     }
     $scope.data = msg.data;
   });
